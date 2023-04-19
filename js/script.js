862a8435d7c84b148c1c7e352afa9edf8fef06e4
Tofunmi Kusimo <tofunmikusimo21@gmail.com>
2023-04-19 23:16:21 +0100

    add script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $("#formOne").submit(function (event){
+
+        const firstNameInput = $("input#first-name").val();
+        const lastNameInput = $("input#last-name").val();
+        const emailInput = $("input#email").val();
+        const planeUseInput = $("select#plane-use").val();
+        const planeServiceInput = $("select#plane-service").val();
+        const colorInput = $("input#color").val();
+        const genderInput = $("input:radio[name=Gender]:checked").val();
+        const nextUseInput = $("input:radio[name=next-use]:checked").val();
+        
+
+        $(".name1").text(firstNameInput);
+        $(".name2").text(lastNameInput);
+        $(".email").text(emailInput);
+        $(".gender").text(genderInput);
+        $(".fColor").text(colorInput);
+        $(".use-regularity").text(planeUseInput);
+        $(".use-experience").text(planeServiceInput);
+        $(".use-again").text(nextUseInput);
+
+
+        $("#summary").show();
+        $(".content").hide();
+        
+        event.preventDefault();
+    });
+
+});
